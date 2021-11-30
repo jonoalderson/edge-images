66a8e291f938161a7f9df4e7db9f50bc7004b950
Jono <jonoalderson@gmail.com>
2021-11-30 12:09:02 +0000

    .

--- classes/class-image.php
+++ classes/class-image.php
@@ -300,7 +300,7 @@ class Image {
 			'format'  => ( $this->has_attr( 'format' ) ) ? $this->get_attr( 'format' ) : null,
 			'quality' => ( $this->has_attr( 'quality' ) ) ? $this->get_attr( 'quality' ) : null,
 		);
-		$cf_src = Helpers::cf_src( $full_image[0], $args );
+		$cf_src = Helpers::cf_src( $this->attrs['full-src'], $args );
 
 		if ( ! $cf_src ) {
 			return; // Bail if the CF src generation fails.
